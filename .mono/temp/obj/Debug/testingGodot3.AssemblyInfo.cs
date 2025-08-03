0645247bbe02346551c8bd46ce9b33213be80095
Andrew jovaras <andrewjovaras@gmail.com>
2025-08-03 17:56:52 +1000

    updating web build files

--- .mono/temp/obj/Debug/testingGodot3.AssemblyInfo.cs
+++ .mono/temp/obj/Debug/testingGodot3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testingGodot3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a4aa91581368692d8bb73345806f5a3cfc63378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7c5679ddc834016b83341aa8e4b9621088e55d")]
 [assembly: System.Reflection.AssemblyProductAttribute("testingGodot3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testingGodot3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
